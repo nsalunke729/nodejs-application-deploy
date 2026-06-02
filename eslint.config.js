10f006dcab88788c1ecdf53deab6b7807e487414
Niranjan <nsalunke729@gmail.com>
2026-06-02 23:36:41 +0100

    lint config added

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import js from '@eslint/js';
+
+export default [
+    js.configs.recommended,
+    {
+        files: ['**/*.js', '**/*.ts'],
+        rules: {
+            'no-unused-vars': 'warn',
+            'no-console': 'off',
+        }
+    }
+];
